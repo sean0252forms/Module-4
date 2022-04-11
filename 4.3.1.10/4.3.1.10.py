35c164b27c1920bcb15b75b211a71cf30a793b67
Sean <75319944+sean0252forms@users.noreply.github.com>
2022-04-11 13:01:13 +0000

    final 4.3.1.10

--- 4.3.1.10/4.3.1.10.py
+++ 4.3.1.10/4.3.1.10.py
@@ -1,12 +1,19 @@
-# def liters_100km_to_miles_gallon(liters):
+# 1 American mile = 1609.344 metres;
+# 1 American gallon = 3.785411784 litres.
+def liters_100km_to_miles_gallon(liters):
 #
 # Write your code here.
 #
-# def miles_gallon_to_liters_100km(miles):
+    gallons =liters/3.785411784
+    miles =100*1000/1609.344
+    return str(liters)+' '+str(miles/gallons)
+def miles_gallon_to_liters_100km(miles):
 #
 # Write your code here
 #
-
+    km100=miles *1609.344/1000/100
+    liters=3.785411784
+    return str(miles)+' '+str(liters/km100)
 print(liters_100km_to_miles_gallon(3.9))
 print(liters_100km_to_miles_gallon(7.5))
 print(liters_100km_to_miles_gallon(10.))
